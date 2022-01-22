1ae860a871702fef3dec2ea42a9dc3ceb6ac11e4
gustavobat <gustavoabatistela@gmail.com>
2022-01-22 12:58:47 -0300

    Extending size of body container

--- src/Body.js
+++ src/Body.js
@@ -2,7 +2,7 @@ import {Row, Col, Container} from 'react-bootstrap';
 
 function Body() {
   return (
-    <Container fluid>
+    <Container fluid style={{backgroundColor: "lightblue",  minHeight: "70vh", minWidth: "100wh"}}>
       <Row>
         <Col xs={{ span: 3, offset: 6 }}>Sections</Col>
       </Row>
