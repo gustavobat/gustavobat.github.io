80c908fe4dd66324b24f5639a1e1a61c9413884d
gustavobat <gustavoabatistela@gmail.com>
2022-01-24 10:44:01 -0300

    Add Container in App component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
-    <>
+    <Container fluid style={{width: "100%", height: "100%", justifyContent: "space-between", padding: "0"}}>
       <Header/>
       <Body/>
       <Footer/>
-    </>
+    </Container>
   );
 }
 
