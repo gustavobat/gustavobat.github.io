e99c5a60f03a83d3e00a9356ebcb121919d0e079
gustavobat <gustavoabatistela@gmail.com>
2022-01-24 10:44:22 -0300

    Change background color of Footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import {Row, Col, Container} from 'react-bootstrap';
 
 function Footer() {
   return (
-    <Container fluid>
+    <Container fluid style={{backgroundColor: "black", color: "white", height: "20vh", width: "100wh", paddingRight: "0"}}>
       <Row>
         <Col xs={{ span: 3, offset: 6 }}>Sections</Col>
         <Col xs={{ span: 3 }}>Social</Col>
