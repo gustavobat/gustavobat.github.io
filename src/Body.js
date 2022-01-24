d448f0c1a11cf3aa43882094280ff4b8f0cf5530
gustavobat <gustavoabatistela@gmail.com>
2022-01-24 10:46:07 -0300

    Add repo cards to body

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,28 @@
 import {Row, Col, Container} from 'react-bootstrap';
+import RepoCard from './RepoCard.tsx'
 
 function Body() {
   return (
-    <Container fluid style={{backgroundColor: "lightblue",  minHeight: "70vh", minWidth: "100wh"}}>
-      <Row>
-        <Col xs={{ span: 3, offset: 6 }}>Sections</Col>
+    <Container style={{height: "100vh", justifyContent: "center", paddingTop: "5vh", paddingBottom: "5vh"}}>
+      <Row style={{height: "100vh", justifyContent: "center", padding: "100"}}>
+        <Col xs={{span: 8}}>
+          <Row>
+            <Col>
+              <RepoCard username="gustavobat" repository="jstatics"/>
+            </Col>
+            <Col>
+              <RepoCard username="gustavobat" repository="gustavobat.github.io"/>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <RepoCard username="gustavobat" repository="strings-n-scales"/>
+            </Col>
+            <Col>
+              <RepoCard username="gustavobat" repository="compile-time-8-queens"/>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
